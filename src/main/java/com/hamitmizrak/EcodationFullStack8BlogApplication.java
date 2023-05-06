eb092275ccc9410bdf334af051b4d1b80ac8fda0
hamitmizrak <hamitmizrak@gmail.com>
2023-05-06 15:49:30 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/EcodationFullStack8BlogApplication.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodationFullStack8BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcodationFullStack8BlogApplication.class, args);
+	}
+
+}
